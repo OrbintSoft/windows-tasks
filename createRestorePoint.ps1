e5378dfbab13b6a05ee5c5fc94f2c644062d3c83
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2024-07-26 17:56:53 +0200

    improve

--- createRestorePoint.ps1
+++ createRestorePoint.ps1
@@ -1,10 +1,10 @@
 $currentDate = Get-Date -Format s
 $restorePointName = "Scheduled Restore Point $currentDate"
-$result = CheckPoint-Computer $restorePointName
+$result = CheckPoint-Computer "$restorePointName"
 $logName = "RestorePoint-Script"
-$logFileExists = [System.Diagnostics.EventLog]::SourceExists($logName);
-if (! $logFileExists) {
-    Write-Host "Log $logNam do not exist, it will be created"
-    New-EventLog -LogName Application -Source $logName
+$logFileExists = [System.Diagnostics.EventLog]::SourceExists("$logName")
+if (!$logFileExists) {
+    Write-Host "Log $logName do not exist, it will be created"
+    New-EventLog -LogName Application -Source "$logName"
 }
-Write-EventLog -LogName Application -Source $logName -EntryType Information -EventID 1 -Message "RestorePoint-Script has been run"
+Write-EventLog -LogName Application -Source "$logName" -EntryType Information -EventID 1 -Message "RestorePoint-Script has been run"
--- syncTime.ps1
+++ syncTime.ps1
@@ -1,7 +1,8 @@
-$result =  w32tm.exe /resync | out-string
-$logName = “SyncTime-Script”
+ï»¿$result = w32tm.exe /resync | out-string
+$logName = "SyncTime-Script"
 $logFileExists = [System.Diagnostics.EventLog]::SourceExists($logName)
-if (! $logFileExists) {
-    New-EventLog –LogName Application –Source $logName
+if (!$logFileExists) {
+	Write-Host "Log $logName do not exist, it will be created"
+	New-EventLog -LogName Application -Source "$logName"
 }
-Write-EventLog –LogName Application –Source $logName –EntryType Information –EventID 1 –Message $result
+Write-EventLog -LogName Application -Source $logName -EntryType Information -EventID 1 -Message $result
